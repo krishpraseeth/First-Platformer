526a60f668a51e60f4052218fbd281ac1e3890ac
krishpraseeth <49354816+krishpraseeth@users.noreply.github.com>
2020-03-21 10:47:28 -0700

    First Level of First Platformer

---
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        if (isGrounded == true)
+        {
+            animator.SetBool("Grounded", true);
+            animator.SetFloat("JumpSpeed", 0);
+
+        }
+        else if (isGrounded == false)
+        {
+            animator.SetBool("Grounded", false);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(moveInput));
+        animator.SetFloat("JumpSpeed", Mathf.Abs(rb.velocity.y));
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Respawn"))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
